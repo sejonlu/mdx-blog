373b10cf9c9f0b7ed254e9ff3221b04ccd947f4a
Jonas Lundahl <Jonas.Lundahl@pipechain.com>
2023-08-11 14:30:42 +0200

    Animated division widget: Rendering embedded components

--- src/components/CustomMDX/CustomMDX.js
+++ src/components/CustomMDX/CustomMDX.js
@@ -2,10 +2,17 @@ import React from "react";
 
 import { MDXRemote } from "next-mdx-remote/rsc";
 
+import dynamic from "next/dynamic";
+
 import CodeSnippet from "@/components/CodeSnippet";
+
+const DivisionGroupsDemo = dynamic(() =>
+  import("@/components/DivisionGroupsDemo")
+);
 
 const components = {
   pre: (props) => <CodeSnippet {...props} />,
+  DivisionGroupsDemo: DivisionGroupsDemo,
 };
 
 function CustomMDX(props) {
